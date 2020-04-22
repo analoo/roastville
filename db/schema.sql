90b24293f0eaef4f5e89e8373a2a6c31a078879a
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-22 15:35:02 -0700

    updated mysql database schema with appropriate syntax and foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,27 @@
+DROP DATABASE IF EXISTS barista_db;
+
 CREATE DATABASE barista_db;
 USE barista_db;
 
-CREATE TABLE orders
-(
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    order int NOT NULL,
-	delivered BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
-
 CREATE TABLE menu(
-    id int NOT NULL AUTO_INCREMENT PRIMARY,
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(70) NOT NULL,
     price decimal(10,2) NOT NULL
 );
 
 CREATE TABLE customer(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(70),
-    total_spend INT,
+    total_spend INT
+);
 
-)
+CREATE TABLE orders
+(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id int NOT NULL,
+    customer_id int,
+	delivered BOOLEAN DEFAULT false,
+    FOREIGN KEY (order_id) REFERENCES menu(id),
+    FOREIGN KEY (customer_id) REFERENCES customer(id)
+
+);
