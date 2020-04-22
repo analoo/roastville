ff43ffd7b63f2e1f116a0cfa85fa7e44b9e8089c
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-22 16:41:54 -0700

    created order CRUD model

---
+++ models/order.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm.js");
+
+var order = {
+    all: function (cb) {
+        orm.all("orders", function (res) {
+            cb(res)
+        });
+    },
+
+    create: function (cols, vals, cb) {
+        orm.create("orders", cols, vals, cb, function (res) {
+            cb(res)
+        });
+    },
+
+    update: function(vals,condition,cb){
+        orm.update("orders", vals, condition, function(res){
+            cb(res);
+        });
+    },
+
+    delete: function(condition, cb){
+        orm.delete("orders", condition, function(res){
+            cb(res)
+        });
+    }
+}
+
+module.exports = order;
