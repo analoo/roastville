4f8d95a21a0506c899499068a17c66ccad3ff636
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-24 23:48:59 -0700

    set restriction that user can only have two active orders and updated customer data so it changes photo every time

--- public/assets/js/cafe.js
+++ public/assets/js/cafe.js
@@ -3,56 +3,61 @@
 $(function () {
     const name = ["Tania", "Cori", "Jon", "Carlos", "Jim", "Tara", "Marcos", "Mark", "Poppe", "Marta", "Ana", "Tom", "Juan", "Farley", "Rori", "Carl", "Kim"];
     const item = ["Latte", "Capuccino", "Matcha Tea", "Drip Coffee"];
-    const image = ["Latte", "Capuccino", "Matcha Tea", "Drip Coffee"];
+    const image = ["/assets/images_icons/tech_bro.png", "/assets/images_icons/music_listener.png", "/assets/images_icons/cyclist.png"];
+    var active = 0;
 
 
-    function rand(lst){
-        return Math.floor(Math.random() * (lst.length-0+1)) 
+    function rand(lst) {
+        return Math.floor(Math.random() * (lst.length - 0 + 1))
     }
 
-    var current_order;
+    var current_order=item[1];
+    var current_img=image[2];
+    var current_name=name[5]
     var earnings = 0;
     var time = 100;
+    $("#cust-name").text(current_name);
+    $("#ord-name").text(current_order);
+    $("#customer").attr("src", current_img);
 
     $("#time-rem").text(time);
     $("#money").text(earnings)
 
 
-    
-    $("#start-game").on("click", function(){
+
+    $("#start-game").on("click", function () {
         event.preventDefault();
-        $("#start").css("display","none");
-        $("#mask").css("display","none");
+        $("#start").css("display", "none");
+        $("#mask").css("display", "none");
 
-        
-        var i =0;
-        
+        var customers = setInterval(() => {
+            current_order = item[rand(item)]
+            current_name= name[rand(name)]
+            current_img = image[rand(image)]
+            $("#cust-name").text(current_name);
+            $("#ord-name").text(current_order);
+            $("#customer").attr("src", current_img);
 
+        }, 8000);
         var gametime = setInterval(() => {
-            time-=1;
+            time -= 1;
             $("#time-rem").text(time);
 
-           if(time == 0){
-            $("#time-rem").text(time);
-               clearInterval(gametime)
-               clearInterval(customers)
-           }
-           
-       }, 1000);
+            if (time == 0) {
+                $("#time-rem").text(time);
+                clearInterval(gametime)
+                clearInterval(customers)
+            }
 
-       var customers = setInterval(() => {
-        current_order = item[rand(item)]
-        $("#cust-name").text(name[rand(name)]);
-        $("#ord-name").text(current_order);
-        i++;
-       
-   }, 8000);
+        }, 1000);
+
+
 
     });
 
     $(".create-form").on("submit", function () {
         event.preventDefault();
-        console.log("Current order is: "+current_order)
+        console.log("Current order is: " + current_order)
 
         var newOrder = {
             item_id: $("#item-id").val().trim(),
@@ -72,55 +77,61 @@ $(function () {
         );
     });
 
-    
 
-    $(document).on("click",".delete", function () {
+
+    $(document).on("click", ".delete", function () {
         event.preventDefault();
-        console.log("you clicked me!")
         var id = $(this).data("id");
-    
-        console.log(id)
-    
+
         $.ajax("/api/orders/" + id, {
             type: "DELETE",
         }).then(
             function () {
-            console.log("You deleted this order");
-            $(`#row-${id}`).remove()
-        });
+                $(`#row-${id}`).remove()
+                active --
+            });
     });
-    
-    
-    $(document).on("click",".add",function(){
+
+
+    $(document).on("click", ".add", function () {
         event.preventDefault();
         var id = $(this).data("id");
         var time = $(`#prog-${id}`).data("time");
-        var comp = 100/time;
-        var incr=0;
+        var comp = 100 / time;
+        var incr = 0;
+        console.log("active is: "+active)
+
+        if(active < 2){
+            active++
+            console.log("active is: "+active)
+
+            var progress = setInterval(() => {
+                incr += comp;
+                $(`#prog-${id}`).attr("value", incr)
+                time--
+                if (time == 0) {
+                    $(`#prog-${id}`).attr("value", 100)
+                    $.ajax("/api/orders/" + id, {
+                        type: "PUT",
+                    }).then(
+                        function () {
+                            $(`#row-${id}`).remove();
+                            active--
+                            console.log("active is: "+active)  
     
-        console.log("This is my id:" +id)
-    
-    
-        var progress = setInterval(() => {
-             incr+= comp;
-            $(`#prog-${id}`).attr("value",incr)
-            time --
-            if(time == 0){
-                $(`#prog-${id}`).attr("value",100)
-                $.ajax("/api/orders/" + id, {
-                    type: "PUT",
-                }).then(
-                    function () {
-                        // earnings+= $(`#row-${id}`).data("price");
-                        // $("#money").text(earnings)
-                        $(`#row-${id}`).remove();
-                        
-                    
-                });
-                clearInterval(progress)
-            }
-            
-        }, 1000);
+                        });
+                    clearInterval(progress)
+                }
+            }, 1000);
+        }
+
+        else{
+            $(`row-${id}`).css("color", "grey")
+
+        }
+
+
+        
     });
 
 
