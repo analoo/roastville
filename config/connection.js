1e3beda868cec8bc81f14747fc333b2413a1f1e3
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-25 20:48:13 -0700

    updated connection to be heroku jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "barista_db"
-  });
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "barista_db"
+//   });
+
+  var connection = mysql.createConnection(process.env.JASWDB_URL);
 
   connection.connect(function(err) {
     if (err) {
